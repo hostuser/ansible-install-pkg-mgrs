2dfc0c1b46aa4feb45f777dd7b6dcba6191a9092
Markus Binsteiner <makkus@gmail.com>
2017-01-10 15:46:42 +1300

    Moved default nix store location.

--- files/install_nix_proot.sh
+++ files/install_nix_proot.sh
@@ -4,6 +4,7 @@ BASE_DIR="$HOME/.freckles"
 PROOT_DIR="$BASE_DIR/opt/proot"
 NIX_DIR="$BASE_DIR/opt/nix"
 NIX_VERSION="1.11.5-x86_64-linux"
+NIX_ROOT="$NIX_DIR/root"
 
 NIX_BINARY_URL="https://nixos.org/releases/nix/latest/nix-$NIX_VERSION.tar.bz2"
 
@@ -19,11 +20,13 @@ wget "$NIX_BINARY_URL"
 tar xvjf nix-*bz2
 rm nix-*bz2
 
-$PROOT_DIR/proot-x86_64 -b $NIX_DIR/nix-$NIX_VERSION/:/nix bash -c "cd /nix; ./install"
+mv "$NIX_DIR/nix-$NIX_VERSION" "$NIX_ROOT"
+
+$PROOT_DIR/proot-x86_64 -b $NIX_DIR/root:/nix bash -c "cd /nix; ./install"
 
 echo "Add:"
 echo
-echo "    $PROOT_DIR/proot-x86_64 -b $NIX_DIR/nix-$NIX_VERSION/:/nix bash"
+echo "    $PROOT_DIR/proot-x86_64 -b $NIX_ROOT/:/nix bash"
 echo "    source $HOME/.nix-profile/etc/profile.d/nix.sh"
 echo
 echo "to your .bashrc to be able to use this nix environment."
